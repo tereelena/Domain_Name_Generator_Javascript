fdef1fc41116eded674815836cf445b76f49a237
tereelena <tereelena@gmail.com>
2021-07-09 02:22:33 +0000

    Code The Domain Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+console.log("hello world");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let x = 0; x < noun.length; x++) {
+      let word = pronoun[i] + adj[j] + noun[x];
+      console.log(word + ".com");
+    } //tercer for
+  } //segundo for
+} // primet for
